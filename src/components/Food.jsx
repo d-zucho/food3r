e85f4d56f2d4403c3612db7765c217646c46e80c
D_Zucho <danny.nico.dev@outlook.com>
2023-08-15 03:20:47 -0700

    finish food items and filter by type and price

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -4,6 +4,24 @@ import { data } from '../data/data'
 function Food() {
   const [foods, setFoods] = useState(data)
 
+  // Filter Tyoe: Burgers, Pizza, Salads, Chicken
+  const filterType = (type) => {
+    setFoods(
+      data.filter((item) => {
+        return type === item.category
+      })
+    )
+  }
+
+  // Filter Price: $, $$, $$$, $$$$
+  const filterPrice = (price) => {
+    setFoods(
+      data.filter((item) => {
+        return price === item.price
+      })
+    )
+  }
+
   return (
     <section id='filter-foods' className='max-width-[1640px] m-auto px-4 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>
@@ -16,19 +34,34 @@ function Food() {
         <div>
           <p className='font-bold my-4 text-gray-700'>Filter Type</p>
           <div className='flex justify-between flex-wrap lg:gap-2'>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => setFoods(data)}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               All
             </button>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterType('burger')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               Burgers
             </button>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterType('pizza')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               Pizza
             </button>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterType('salad')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               Salads
             </button>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterType('chicken')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               Chicken
             </button>
           </div>
@@ -38,16 +71,28 @@ function Food() {
         <div>
           <p className='font-bold my-4 text-gray-700'>Filter Price</p>
           <div className='flex justify-between max-w-[390px] w-full lg:gap-2'>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterPrice('$')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               $
             </button>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterPrice('$$')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               $$
             </button>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterPrice('$$$')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               $$$
             </button>
-            <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+            <button
+              onClick={() => filterPrice('$$$$')}
+              className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'
+            >
               $$$$
             </button>
           </div>
@@ -55,13 +100,25 @@ function Food() {
       </div>
 
       {/* Display Foods */}
-      <section id='food-items' className=''>
-        {foods.map((item) => (
-          <div>
-            <img src={item.image} alt={item.name} />
-            <div>
-              <p>{item.name}</p>
-              <p>{item.price}</p>
+      <section
+        id='food-items'
+        className='grid grid-cols-2 lg:grid-cols-3 gap-6 pt-4'
+      >
+        {foods.map((item, index) => (
+          <div
+            key={index}
+            className='border rounded-lg shadow-lg hover:scale-105 duration-300 pb-2'
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className='w-full h-[200px] object-cover rounded-t-lg'
+            />
+            <div className='px-2 flex justify-between py-2'>
+              <p className='font-bold'>{item.name}</p>
+              <span className='bg-orange-600 text-white p-1 rounded-full'>
+                {item.price}
+              </span>
             </div>
           </div>
         ))}
